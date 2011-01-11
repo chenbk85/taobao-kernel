7db02c19246c5fb45fe5f06ccfa85ec966613735
Zhu Yanhai (Gao Yang) <gaoyang.zyh@taobao.com>
2011-01-11 15:40:03 +0800

    - rpm/taobao-kernel-build.sh: build without perftool.

--- rpm/taobao-kernel-build.sh
+++ rpm/taobao-kernel-build.sh
@@ -3,5 +3,5 @@
 cd $1
 python ./scripts/package.py
 cd taobao-kernel-build
-rpmbuild -bb  --rmsource *.spec --without fips --without kabichk --without perf --without xen --define="_rpmdir $1/taobao-kernel-build/rpm" --define="_builddir $1/taobao-kernel-build" --define="_sourcedir $1/taobao-kernel-build" --define="_tmppath $1/taobao-kernel-build/rpm"
+rpmbuild -bb  --rmsource *.spec --without fips --without kabichk --without perftool --without xen --define="_rpmdir $1/taobao-kernel-build/rpm" --define="_builddir $1/taobao-kernel-build" --define="_sourcedir $1/taobao-kernel-build" --define="_tmppath $1/taobao-kernel-build/rpm"
 
