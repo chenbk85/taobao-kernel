31c844df6d09d080778fcfb9dbb7f0ab5b434078
Zhu Yanhai (Gao Yang) <gaoyang.zyh@taobao.com>
2011-01-14 17:55:17 +0800

    - Update config files. Turn on the deprecated options for rhel5. - rpm/kernel.spec.in: disable various package requires on old rhel. - rpm/taobao-kernel-build.sh: we don't use dracut.

--- rpm/taobao-kernel-build.sh
+++ rpm/taobao-kernel-build.sh
@@ -4,5 +4,5 @@ sudo yum install perl-TimeDate -y
 cd $1
 python ./scripts/package.py
 cd taobao-kernel-build
-rpmbuild -bb  --rmsource *.spec --without debug --without fips --without kabichk --without perftool --without xen --define="_rpmdir $1/rpm" --define="_builddir $1/taobao-kernel-build" --define="_sourcedir $1/taobao-kernel-build" --define="_tmppath $1/rpm"
+rpmbuild -bb  --rmsource *.spec --without dracut --without debug --without fips --without kabichk --without perftool --without xen --define="_rpmdir $1/rpm" --define="_builddir $1/taobao-kernel-build" --define="_sourcedir $1/taobao-kernel-build" --define="_tmppath $1/rpm"
 
