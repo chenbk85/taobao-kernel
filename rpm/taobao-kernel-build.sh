a068aa06b09fa26235c468a966092d065f4f4836
Advertisements <ads@xendev4-vm7.corp.alimama.com>
2011-01-14 10:31:57 +0800

    modify the sh for abs

--- rpm/taobao-kernel-build.sh
+++ rpm/taobao-kernel-build.sh
@@ -5,6 +5,9 @@ cd $1
 python ./scripts/package.py
 cd taobao-kernel-build
 rpmbuild -bb  --rmsource *.spec --without debug --without fips --without kabichk --without perftool --without xen --define="_rpmdir $1/rpm" --define="_builddir $1/taobao-kernel-build" --define="_sourcedir $1/taobao-kernel-build" --define="_tmppath $1/rpm"
+
+cd $1/rpm
+find . -name "*.rpm"  -exec mv {} . \;
 for mypk in `ls *.rpm`
 do
 	t_pk_l=${mypk//-/ }
