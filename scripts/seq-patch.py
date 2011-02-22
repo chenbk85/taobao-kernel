d79bf78c150616c12006bc418fd8915b2f1bed23
Zhu Yanhai (Gao Yang) <gaoyang.zyh@taobao.com>
2011-02-22 14:22:05 +0800

    - scripts/seq-patch.py: insert a forked check_output method to subprocess if it doesn't exist.

--- scripts/seq-patch.py
+++ scripts/seq-patch.py
@@ -54,6 +54,38 @@ def rm_in_background (folder):
         subprocess.call(["rm", "-rf", folder])
 
 
+def local_check_output(*popenargs, **kwargs):
+    r"""Run command with arguments and return its output as a byte string.
+
+    If the exit code was non-zero it raises a CalledProcessError.  The
+    CalledProcessError object will have the return code in the returncode
+    attribute and output in the output attribute.
+
+    The arguments are the same as for the Popen constructor.  Example:
+
+    >>> check_output(["ls", "-l", "/dev/null"])
+    'crw-rw-rw- 1 root root 1, 3 Oct 18  2007 /dev/null\n'
+
+    The stdout argument is not allowed as it is used internally.
+    To capture standard error in the result, use stderr=STDOUT.
+
+    >>> check_output(["/bin/sh", "-c",
+    ...               "ls -l non_existent_file ; exit 0"],
+    ...              stderr=STDOUT)
+    'ls: non_existent_file: No such file or directory\n'
+    """
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
+
 def  check_patch_version ():
     # We were told that some patches require patch 2.5.4
     version_out = subprocess.check_output(["patch", "--version"])
@@ -120,6 +152,10 @@ if __name__ == "__main__":
     if not check_patch_version():
         print >>sys.stderr, "Patch version >= 2.5.4 is required.\n"
         sys.exit(1)
+
+    # To compatbile with older python (<2.7)
+    if not 'check_output' in dir(subprocess):
+        subprocess.check_output = local_check_output
 
     stored_dir = os.getcwd()
     os.chdir(get_script_loc())
