f8d176609c76f4356140b88d76589f67063f3705
GH042718-ZhuYanHai <gaoyang.zyh@mmdev1.corp.alimama.com>
2011-03-10 22:31:56 +0800

    - rpm/taobao-kernel-build.sh: enable perf build.

--- rpm/taobao-kernel-build.sh
+++ rpm/taobao-kernel-build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ##for check
-sudo yum install rng-utils m4 python-devel perl-TimeDate binutils-devel zlib-devel elfutils-libelf-devel -y
+sudo yum install rng-utils m4 python26-devel python-devel perl-TimeDate binutils-devel zlib-devel elfutils-libelf-devel -y
 sudo pkill -9 rngd
 sudo rngd -r /dev/urandom
 cd $1
 echo Starting "$BUILD_NUMBER"th Taobao-kernel build.
 python ./scripts/package.py --buildid $BUILD_NUMBER
 cd taobao-kernel-build
-rpmbuild -bb  --rmsource *.spec --without lvs --without perf --without perftool --without dracut --without debug --define="_rpmdir $1/rpm" --define="_builddir $1/taobao-kernel-build" --define="_sourcedir $1/taobao-kernel-build" --define="_tmppath $1/rpm"
+rpmbuild -bb  --rmsource *.spec --without lvs --without dracut --without debug --define="_rpmdir $1/rpm" --define="_builddir $1/taobao-kernel-build" --define="_sourcedir $1/taobao-kernel-build" --define="_tmppath $1/rpm"
 
 cd $1/rpm
 find . -name "*.rpm"  -exec mv {} . \;
