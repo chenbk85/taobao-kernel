c38dcdd7e6aa89850d43a7a422efd3794c5e047f
Coly Li <bosong.ly@taobao.com>
2011-06-03 17:03:46 +0800

    - rpm/kernel.spec.in: Add packager as gaoyang.zyh@taobao.com - rpm/taobao-kernel-build.sh: sync from 6u modification.

--- rpm/taobao-kernel-build.sh
+++ rpm/taobao-kernel-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 ##for check
 sudo yum -b current install asciidoc -y
-sudo yum install xmlto m4 python-devel perl-TimeDate binutils-devel zlib-devel elfutils-libelf-devel -y
+sudo yum install xmlto rng-utils m4 python-devel perl-TimeDate binutils-devel zlib-devel elfutils-libelf-devel -y
+sudo pkill -9 rngd
+sudo rngd -r /dev/urandom
 cd $1
 echo Starting "$BUILD_NUMBER"th Taobao-kernel build.
 python ./scripts/package.py --buildid $BUILD_NUMBER
